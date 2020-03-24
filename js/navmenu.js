676ca0ef7c77f2405056aab85c95eda0879a59aa
Kabanoik <61022381+Kabanoik@users.noreply.github.com>
2020-03-24 18:47:43 +0300

    im commit

---
+++ js/navmenu.js
@@ -0,0 +1,12 @@
+$('.dws-menu li > ul').hide();
+
+	$('.services').hover( function(){
+		$('.sub1').slideToggle(300)	
+});
+
+	$('li .subb1').hover(function(){
+		$('#subb2').slideToggle(300)
+});
+	$('li .repairs').hover(function(){
+		$('.sub3').slideToggle(300)
+	});
